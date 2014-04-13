e8c869d1a69b6ed57493ebeac9ebc78e236a82e3
EmilyOwens <emily.f.owens@gmail.com>
2014-04-13 04:54:25 -0600

    Modified About page title

--- spec/requests/major_pages_spec.rb
+++ spec/requests/major_pages_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe "Major Pages" do
   
   describe "Home page" do
-    it "should have the content 'Test Content'" do
+    it "should have the content 'Welcome to the Event Website!'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/major_pages/home'
-      expect(page).to have_content('Test Content')
+      expect(page).to have_content('Welcome to the Event Website!')
     end
     
     it "should have the title 'Home'" do
@@ -19,6 +19,23 @@ describe "Major Pages" do
     it "should have the content 'About Us' " do
       visit '/major_pages/about'
       expect(page).to have_content('About Us')
+    end
+    
+    it "should have the title 'About Us'" do
+      visit '/major_pages/about'
+      expect(page).to have_title("Event Website | About Us")
+    end
+  end
+  
+  describe "Contact page" do
+    it "should have the content 'Contact' " do
+      visit '/major_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    
+    it "should have the title 'Contact'" do
+      visit '/major_pages/contact'
+      expect(page).to have_title("Event Website | Contact")
     end
   end
   
