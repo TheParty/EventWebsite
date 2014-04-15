128b8777e2f94c3749a9b5993f27a8f49e8cbcce
Abebe <abta3355@colorado.edu>
2014-04-15 14:12:39 -0600

    good starting point

---
+++ features/step_definitions/createEvent_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am in the Edit Event page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill in the create event form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be able to invite other users$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my event should be listed in the events list$/) do
+  pending # express the regexp above with the code you wish you had
+end
