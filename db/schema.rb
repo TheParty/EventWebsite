9c2b898cc3807fcb8ce6dc18cc5123c38a0a6983
EmilyOwens <emily.f.owens@gmail.com>
2014-04-16 04:09:32 -0600

    Added secure passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416093001) do
+ActiveRecord::Schema.define(version: 20140416094339) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140416093001) do
     t.integer  "futureEvents"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
