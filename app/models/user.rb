15e70807b0e6ec44fa2c874499db51de8ee5668c
EmilyOwens <emily.f.owens@gmail.com>
2014-04-16 03:39:31 -0600

    Added email verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true
-  validates :email, presence: true
+  before_save { self.email = email.downcase }
+  
+  validates :username, presence: true, length: { maximum: 50 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+            uniqueness: { case_sensitive: false}
   
   has_many :events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140413092027) do
+ActiveRecord::Schema.define(version: 20140416093001) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -34,5 +34,7 @@ ActiveRecord::Schema.define(version: 20140413092027) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
